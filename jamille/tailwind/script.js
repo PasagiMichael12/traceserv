271c8a96dc17343fe060d8cc996917c6494baf15
unknown <santosjamillejoynicole@gmail.com>
2021-05-19 18:42:21 +0800

    tailwind

---
+++ jamille/tailwind/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".card1").on('click', function(){
+        $(".card1").toggleClass('bg-purple-500 text-white');
+    });
+    $(".card2").on('click', function(){
+        $(".card2").toggleClass('bg-purple-500 text-white');
+    });
+    $(".card3").on('click', function(){
+        $(".card3").toggleClass('bg-purple-500 text-white');
+    });
+    $(".card4").on('click', function(){
+        $(".card4").toggleClass('bg-purple-500 text-white');
+    });
+    $(".card5").on('click', function(){
+        $(".card5").toggleClass('bg-purple-500 text-white');
+    });
+    $(".card6").on('click', function(){
+        $(".card6").toggleClass('bg-purple-500 text-white');
+    });
+});
